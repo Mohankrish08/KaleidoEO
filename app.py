a6fb6f80123461372d258c1ddee62335713ab12c
Mohankrish08 <mohankrishce@gmail.com>
2023-12-08 23:33:19 +0530

    streamlit

---
+++ app.py
@@ -0,0 +1,46 @@
+# importing libraries
+
+import streamlit as st
+import streamlit_lottie as lottie
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from inference import inferencing_model
+from PIL import Image
+import os
+import requests
+
+# set up the page
+st.set_page_config(page_title='KaleidEO', page_icon=":computer:", layout='wide')
+
+# loading animations
+
+def loader_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# lottie files
+
+icon = loader_url('https://lottie.host/395bfca4-62be-47a0-9a89-c6abade8d690/ZLRP88UkIM.json')
+
+
+# main code
+
+st.markdown("<h1 style='text-align: center;'>KaleidEO: MLOps Engineer Assignment</h1>", unsafe_allow_html=True)
+
+
+with st.container():
+    i,j = st.columns((1,2))
+    with i:
+        st.lottie(icon, height=300, key='icon')
+    with j:
+        photo = st.file_uploader('Upload the image', type=['jpg','jpeg','png','tiff'])
+        if st.button('Enter') and photo is not None:
+            inferencing_model(photo)
+            st.image('predictions.jpg')
+
+            os.remove('sample.jpg')
+            os.remove('predictions.jpg')
